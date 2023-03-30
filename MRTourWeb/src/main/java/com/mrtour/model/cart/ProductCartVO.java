37ca8200f851df6d83edb2addf89e0829e4c1e71
YOUNGJEE SEO <dldwnlsj13@gmail.com>
2023-03-30 15:37:35 +0900

    변경사항 커밋(장바구니 구현 중)

--- MRTourWeb/src/main/java/com/mrtour/model/cart/ProductCartVO.java
+++ MRTourWeb/src/main/java/com/mrtour/model/cart/ProductCartVO.java
@@ -3,28 +3,19 @@ package com.mrtour.model.cart;
 import java.util.List;
 
 public class ProductCartVO {
-	private String cate_id; // 상품 종류 코드 (FK)
-	private String prd_name; // 상품명
-	private String prd_opt; // 상품 옵션	
+
+	private String prd_name; // 상품명	
 	private int prd_price; // 상품 가격
 	private int prd_sum; // 가격 합계
-	private String prd_img; // 상품 이미지
+
 	private String member_id; // 회원 아이디(FK)
 	private String prd_id; // 상품 코드(FK)
 	private int buy_quantity; // 주문 수량
-	private int cart_no; // 장바구니 고유번호
-
-	private List<String> cartPayList; // 장바구니 리스트
-
+	private int cart_id; // 장바구니 고유번호 
+	private String cate_id; // 상품 종류 코드(FK)
+	private String prd_opt; // 상품 옵션
 	
-
-	public String getCate_id() {
-		return cate_id;
-	}
-
-	public void setCate_id(String cate_id) {
-		this.cate_id = cate_id;
-	}
+	private List<String> cartPayList; // 장바구니 리스트
 
 	public String getPrd_name() {
 		return prd_name;
@@ -34,14 +25,6 @@ public class ProductCartVO {
 		this.prd_name = prd_name;
 	}
 
-	public String getPrd_opt() {
-		return prd_opt;
-	}
-
-	public void setPrd_opt(String prd_opt) {
-		this.prd_opt = prd_opt;
-	}
-
 	public int getPrd_price() {
 		return prd_price;
 	}
@@ -49,7 +32,7 @@ public class ProductCartVO {
 	public void setPrd_price(int prd_price) {
 		this.prd_price = prd_price;
 	}
-	
+
 	public int getPrd_sum() {
 		return prd_sum;
 	}
@@ -82,12 +65,28 @@ public class ProductCartVO {
 		this.buy_quantity = buy_quantity;
 	}
 
-	public int getCart_no() {
-		return cart_no;
+	public int getCart_id() {
+		return cart_id;
 	}
 
-	public void setCart_no(int cart_no) {
-		this.cart_no = cart_no;
+	public void setCart_id(int cart_id) {
+		this.cart_id = cart_id;
+	}
+
+	public String getCate_id() {
+		return cate_id;
+	}
+
+	public void setCate_id(String cate_id) {
+		this.cate_id = cate_id;
+	}
+
+	public String getPrd_opt() {
+		return prd_opt;
+	}
+
+	public void setPrd_opt(String prd_opt) {
+		this.prd_opt = prd_opt;
 	}
 
 	public List<String> getCartPayList() {
